d040a58a10818b6c98f10ac760829cdba8906529
Nick Cabatoff <ncabatoff@hashicorp.com>
2019-04-03 17:54:20 -0400

    Merge terraform into a single file, remove explicit addresses in favour of environment vars.  Remove grafana stuff which has now been factored out into its own repo.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,16 @@
 terraform {
   backend "consul" {
-    path = "terraform"
-    address = "consul.service.dc1.consul:8500"
+    path = "terraform-ncabatoff-jobs"
   }
 }
+
+provider "nomad" {}
+
+resource "nomad_job" "mediaserver" {
+  jobspec = "${file("nomad/mediaserver.hcl")}"
+}
+
+resource "nomad_job" "system-monitoring" {
+  jobspec = "${file("nomad/system-monitoring.hcl")}"
+}
+
